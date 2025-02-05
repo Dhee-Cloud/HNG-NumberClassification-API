3acc8eaac7800acc79df76e7b6952dc0ca2efa42
Uduakabasi Umo-odiong <uduakabasiasuquo6@gmail.com>
2025-02-05 20:44:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
-app = Flask(__name__)
+app = Flask(_name_)
 CORS(app)
 # Helper functions to determine properties of a number
 def is_prime(n):
@@ -55,6 +55,6 @@ def classify_number():
         "fun_fact": fun_fact
     }
     return jsonify(response), 200
-if __name__ == '__main__':
+if _name_ == '_main_':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
