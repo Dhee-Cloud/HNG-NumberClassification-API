9b60750d4cc7bb6f0fdb2e3716f2ecaa49b9fafc
Uduakabasi Umo-odiong <uduakabasiasuquo6@gmail.com>
2025-02-05 17:04:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def classify_number():
 
 if _name_ == '_main_':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
